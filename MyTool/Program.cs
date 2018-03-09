428f4c69d736bd2a981060041907efec04c7a29c
MeirinRuan <36739368+MeirinRuan@users.noreply.github.com>
2018-03-09 16:28:49 +0800

    update
    update the exceloperation

--- MyTool/Program.cs
+++ MyTool/Program.cs
@@ -16,7 +16,7 @@ namespace MyTool
          {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-             Application.Run(new TenRollForm());
+             Application.Run(new LuaShopForm());
         }
     }
 }

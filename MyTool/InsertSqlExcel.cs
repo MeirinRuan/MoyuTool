fda75e4b00771536c91723820d5acfb2c29feabd
MeirinRuan <36739368+MeirinRuan@users.noreply.github.com>
2019-04-17 21:39:32 +0800

    update
    use thread to update insertform tool

--- MyTool/InsertSqlExcel.cs
+++ MyTool/InsertSqlExcel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Windows.Forms;
 using System.IO;
 using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MyTool
 {
@@ -45,6 +47,8 @@ namespace MyTool
         {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
+
+
             //表格索引
             int SheetIndex = Convert.ToInt32(sheetindex_textBox.Text) - 1;
             string TableNameIndex = tablename_textBox.Text;
@@ -54,6 +58,12 @@ namespace MyTool
             //    MessageBox.Show("不存在该sheet。");
             //    return;
             //}
+
+            if (eo.workbook == null)
+            {
+                MessageBox.Show("不存在该工作簿。");
+                return;
+            }
 
             eo.sheet = eo.workbook.GetSheetAt(SheetIndex);
 
@@ -96,22 +106,56 @@ namespace MyTool
 
             progressBar1.Value += 5;
 
-            int CurValue = 35;
+            int CurValue = 80;
             int CurStep = 1;
-            for (int i = 0; i < Rows; i++)
+
+
+            //试着开个线程
+            //int startindex, int RowLength, int ValueStartIndex, int TableFieldCount
+            int startindex = 0;
+            int RowLength1 = 0;
+            int RowLength2 = 0;
+            //int valuestartindex = ValueStartIndex;
+            //int tablefieldcount = TableField.Count;
+
+            //超过1000行，对半处理
+            if (Rows >= 1000)
             {
-                List<string> values = eo.GetRowValues(eo.sheet, i + ValueStartIndex, TableField.Count);
-                ValueList.Add(values);
-                
-                if ( i == Math.Ceiling(Convert.ToDecimal((CurStep / CurValue) * Rows)))
+                startindex = Rows / 2;
+                RowLength1 = startindex;
+                RowLength2 = Rows;
+
+                Func<int, int, int, int, List<List<string>>> t1 = ThreadGetValueList;
+                Func<int, int, int, int, List<List<string>>> t2 = ThreadGetValueList;
+
+                IAsyncResult result1 = t1.BeginInvoke(0, RowLength1, ValueStartIndex, TableField.Count, null, null);
+                IAsyncResult result2 = t2.BeginInvoke(startindex, RowLength2, ValueStartIndex, TableField.Count, null, null);
+                List<List<string>> valuelist1 = t1.EndInvoke(result1);
+                List<List<string>> valuelist2 = t2.EndInvoke(result2);
+
+                valuelist1.AddRange(valuelist2);
+
+                ValueList = valuelist1;
+            }
+            else
+            {
+                RowLength1 = Rows;
+                for (int i = 0; i < RowLength1; i++)
                 {
-                    progressBar1.Value += 1;
-                    CurStep++;
+                    List<string> values = eo.GetRowValues(eo.sheet, i + ValueStartIndex, TableField.Count);
+                    ValueList.Add(values);
+
                 }
             }
 
-            CurValue = 50;
+            
+
+            progressBar1.Value = 95;
+            CurValue = 5;
             CurStep = 1;
+
+            
+
             //获取每一行的数据
             for (int index = 0; index < ValueList.Count; index++)
             {
@@ -130,8 +174,6 @@ namespace MyTool
                 }
             }
 
-
-
             stopwatch.Stop();
 
             //桌面路径
@@ -142,13 +184,31 @@ namespace MyTool
 
             progressBar1.Value = progressBar1.Maximum;
 
-            MessageBox.Show("导出成功，已在桌面生成，耗时：" + stopwatch.Elapsed);
+            MessageBox.Show("导出成功，已在桌面生成。\r\n总耗时：" + stopwatch.Elapsed.TotalSeconds);
 
+            
 
             //生成
             //eo.CreateInsertSql(eo.wb.Worksheets[SheetIndex], SheetIndex.ToString(), TableNameIndex);
 
             //eo.CreateUpdateSql(eo.wb.Worksheets[1], ExcelRangeStart, ExcelRangeEnd, ExcelSetStart, ExcelSetEnd, UpdateStr, SetFieldStr, WhereFieldStr);
+        }
+
+        /// <summary>
+        /// 开启线程执行
+        /// </summary>
+        public List<List<string>> ThreadGetValueList(int startindex, int RowLength, int ValueStartIndex, int TableFieldCount)
+        {
+            
+            List<List<string>> valuelist = new List<List<string>>();
+            for (int i = startindex; i < RowLength; i++)
+            {
+                List<string> values = eo.GetRowValues(eo.sheet, i + ValueStartIndex, TableFieldCount);
+                valuelist.Add(values);
+                Console.WriteLine("i:" + i);
+            }
+
+            return valuelist;
         }
 
     }

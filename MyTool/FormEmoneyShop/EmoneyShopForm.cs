057a928f049132327b19bb8a95f0886065a2aab8
MeirinRuan <36739368+MeirinRuan@users.noreply.github.com>
2020-03-25 21:06:34 +0800

    Revert "update"
    This reverts commit d750612a016a2c08183620e20421576e8bac18a3.

--- MyTool/FormEmoneyShop/EmoneyShopForm.cs
+++ MyTool/FormEmoneyShop/EmoneyShopForm.cs
@@ -112,8 +112,8 @@ namespace MyTool.FormEmoneyShop
                 return;
             }
 
-            string itemtypeId = flowLayoutPanel_wnd.Controls[0].Controls["textBox_itemtype"].Text;
-            string goodsId = flowLayoutPanel_wnd.Controls[0].Controls["textBox_goods"].Text;
+            string goodsId = flowLayoutPanel_wnd.Controls[0].Controls["textBox_itemtype"].Text;
+            string itemtypeId = flowLayoutPanel_wnd.Controls[0].Controls["textBox_goods"].Text;
 
             DateTimePicker dateTimePicker_begintime = (DateTimePicker)flowLayoutPanel_wnd.Controls[0].Controls["dateTimePicker_begintime"];
             DateTimePicker dateTimePicker_endtime = (DateTimePicker)flowLayoutPanel_wnd.Controls[0].Controls["dateTimePicker_endtime"];
